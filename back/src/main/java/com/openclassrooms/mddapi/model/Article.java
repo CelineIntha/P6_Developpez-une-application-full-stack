429668d08cd008efe2d6e8cf2b76a4a62aa05c89
Céline <celine.intha@gmail.com>
2025-02-23 12:45:24 +0100

    - improve and enforce data integrity with constraints on all models

--- back/src/main/java/com/openclassrooms/mddapi/model/Article.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Article.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.mddapi.model;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -11,19 +13,27 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Le titre est obligatoire")
+    @Column(nullable = false)
     private String title;
+
+    @NotBlank(message = "Le contenu est obligatoire")
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, updatable = false)
     private Date createdAt = new Date();
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "author_id", nullable = false)
     private User author;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "topic_id", nullable = false)
     private Topic topic;
 
-    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 
     public Article() {}
@@ -35,61 +45,5 @@ public class Article {
         this.createdAt = createdAt;
         this.author = author;
         this.topic = topic;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-
-    public Topic getTopic() {
-        return topic;
-    }
-
-    public void setTopic(Topic topic) {
-        this.topic = topic;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
     }
 }
--- back/src/main/java/com/openclassrooms/mddapi/model/Comment.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Comment.java
@@ -1,8 +1,7 @@
 package com.openclassrooms.mddapi.model;
 
 import jakarta.persistence.*;
-
-
+import jakarta.validation.constraints.NotBlank;
 import java.util.Date;
 
 @Entity
@@ -11,14 +10,20 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Le contenu est obligatoire")
+    @Column(nullable = false)
     private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, updatable = false)
     private Date createdAt = new Date();
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "author_id", nullable = false)
     private User author;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "article_id", nullable = false)
     private Article article;
 
@@ -29,46 +34,6 @@ public class Comment {
         this.content = content;
         this.createdAt = createdAt;
         this.author = author;
-        this.article = article;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-
-    public Article getArticle() {
-        return article;
-    }
-
-    public void setArticle(Article article) {
         this.article = article;
     }
 }
