4f050d624531cb0c8905b30bd0515fef286ab140
Céline <celine.intha@gmail.com>
2025-02-23 12:45:24 +0100

    - improve and enforce data integrity with constraints on all models

--- back/src/main/java/com/openclassrooms/mddapi/model/Subscription.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Subscription.java
@@ -2,19 +2,20 @@ package com.openclassrooms.mddapi.model;
 
 import jakarta.persistence.*;
 
-
 @Entity
-@Table(name = "subscriptions")
+@Table(name = "subscriptions", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "topic_id"})
+})
 public class Subscription {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "topic_id", nullable = false)
     private Topic topic;
 
@@ -26,27 +27,12 @@ public class Subscription {
         this.topic = topic;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
 
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Topic getTopic() {
-        return topic;
-    }
-
-    public void setTopic(Topic topic) {
-        this.topic = topic;
-    }
+    public Topic getTopic() { return topic; }
+    public void setTopic(Topic topic) { this.topic = topic; }
 }
--- back/src/main/java/com/openclassrooms/mddapi/model/Topic.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Topic.java
@@ -1,10 +1,11 @@
 package com.openclassrooms.mddapi.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.util.ArrayList;
 import java.util.List;
-
 
 @Entity
 @Table(name = "topics")
@@ -12,9 +13,13 @@ public class Topic {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Le nom du sujet est obligatoire")
+    @Size(max = 100, message = "Le nom du sujet ne peut pas dépasser 100 caractères")
+    @Column(unique = true, nullable = false)
     private String name;
 
-    @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Article> articles = new ArrayList<>();
 
     public Topic() {}
@@ -24,27 +29,12 @@ public class Topic {
         this.name = name;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Article> getArticles() {
-        return articles;
-    }
-
-    public void setArticles(List<Article> articles) {
-        this.articles = articles;
-    }
+    public List<Article> getArticles() { return articles; }
+    public void setArticles(List<Article> articles) { this.articles = articles; }
 }
