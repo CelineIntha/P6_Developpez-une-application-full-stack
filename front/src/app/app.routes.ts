994631fb49189895e9a75fed9eff74f2bf21070c
Céline <celine.intha@gmail.com>
2025-03-09 19:09:16 +0100

    - Refactor: 'pages' -> 'features', added app.config.ts for standalone components, update routing - Added eslint

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './features/home/home.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
-const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutes {}
