a4c8d9d5133a2bd78983f7d201dbb6547bb898ba
Céline <celine.intha@gmail.com>
2025-03-11 22:22:00 +0100

    Configured application settings and environment variables

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,11 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import {provideHttpClient} from "@angular/common/http";
-import {provideAnimations} from "@angular/platform-browser/animations";
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),
-    provideAnimations()]
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes),
+    { provide: AuthInterceptor, useClass: AuthInterceptor }
+  ]
 };
