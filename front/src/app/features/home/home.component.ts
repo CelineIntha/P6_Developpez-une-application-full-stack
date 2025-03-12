1fe628d6a2f7926fbdfe3ac408d56acb80591e36
Céline <celine.intha@gmail.com>
2025-03-12 17:35:48 +0100

    Added home and login components

--- front/src/app/features/home/home.component.ts
+++ front/src/app/features/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
     standalone: true,
-    imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, RouterLink],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
