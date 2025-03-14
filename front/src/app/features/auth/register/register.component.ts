381b9a1328d8704787c6c0b66f40a0b9f89afe13
Céline <celine.intha@gmail.com>
2025-03-14 11:17:08 +0100

    created register component

---
+++ front/src/app/features/auth/register/register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent { }
