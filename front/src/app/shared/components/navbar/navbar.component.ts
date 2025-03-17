109416a748649893b73fecaf81337b14e49b31bd
Céline Inthaaphay <celine.inthaaphay@stagiairesmns.fr>
2025-03-17 18:55:45 +0100

    work in progress : improving navbar layout

--- front/src/app/shared/components/navbar/navbar.component.ts
+++ front/src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {NgClass, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    NgClass,
+    NgOptimizedImage
+  ],
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  isNavbarOpen: boolean = false;
+
+  toggleNavbar(): void {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
 }
